adaa3084863aa7cac21622940cc0cec7b2f24976
Harold Cooper <hrldcpr@gmail.com>
2016-09-05 19:22:52 -0400

    init

---
+++ nearest.py
@@ -0,0 +1,28 @@
+import heapq
+import itertools
+
+import numpy as np
+from scipy.spatial.distance import cosine
+from tqdm import tqdm
+
+vectors = {}
+with open('glove.6B/glove.6B.50d.txt') as f:
+    for line in tqdm(f, total=400000):
+        word, *vector = line.split()
+        vectors[word] = np.array([float(x) for x in vector])
+        assert len(vectors[word]) == 50
+
+def get_nearest(v, N=10):
+    nearest = []
+    for word, u in tqdm(vectors.items()):
+        nearness = -cosine(v, u)
+        if len(nearest) < N:
+            heapq.heappush(nearest, (nearness, word))
+        else:
+            heapq.heappushpop(nearest, (nearness, word))
+    return sorted(nearest, reverse=True)
+
+river_bank = vectors['river'] + vectors['bank']
+
+for nearness, word in get_nearest(river_bank):
+    print(word, nearness)
